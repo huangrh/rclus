6521ec6b5fe4af3c4ddaca5c2b3958aea4fbcd70
Ren-Huai Huang <ren-huai.huang@ascension.org>
2017-04-20 15:39:18 -0500

    backup

--- R/rating.R
+++ R/rating.R
@@ -2,32 +2,32 @@
 #'
 #' Hospital star rating from wisorized summary scores.
 #'
-#' @param x Winsorized summary score returned from relvm2 which contains the
-#'   wisorized summary score.
+#' @param x Winsorized summary score returned from relvm.
 #' @param method Kmean cluster algorithm.\itemize{ \item kmeans: kmeans
 #'   clustering. \item rclus: rapid clustering in rclus package.}
 #' @param score_col The column name in a data frame as input for clustering.
+#' @param iter.max The maximum number of iterations.
 #' @return Hospital rating stars 1, 2, 3, 4, and 5. star 5 indicates the best
 #'   hospital in USA.
 #' @seealso \code{\link{rating.data.frame}}
 #' @export
-rating <- function(x,method,score_col) UseMethod("rating")
+rating <- function(x,method,score_col,iter.max) UseMethod("rating")
 
 
 #' @rdname rating
 #'
 #' @export
-rating.default <- function(x, method = c("rclus","kmeans","na")) {
+rating.default <- function(x, method = c("rclus","kmeans","na"),iter.max=200) {
     if (is.null(x)) return(NULL);
 
     star <- switch(method[1],
                    kmeans={
-                       fit = kmeans(x, centers=5, iter.max=100, nstart = 50)
+                       fit = kmeans(x, centers=5, iter.max=iter.max, nstart = 50)
                        fit = cen2star(fit)
                        fit$star
                    },
                    rclus={
-                       fit = rclus(x,seeds=5,maxiter = 200)
+                       fit = rclus(x,seeds=5,maxiter = iter.max)
                        fit$cluster
                    },
                    na={
@@ -40,23 +40,24 @@ rating.default <- function(x, method = c("rclus","kmeans","na")) {
 #'
 #' Hospital star rating from wisorized summary scores in a data frame.
 #'
-#' @param x A data frame contains Winsorized summary score returned from relvm2
-#'   which contains the wisorized summary score.
+#' @param x A data frame containing winsorized summary score returned from relvm.
 #' @param method Kmean cluster algorithm.\itemize{ \item kmeans: kmeans
 #'   clustering. \item rclus: rapid clustering in rclus package.}
 #' @param score_col The column name in a data frame serving as input for
 #'   clustering.
+#' @param iter.max The maximum number of iterations.
 #' @return A data.frame containing the hospital rating stars 1, 2, 3, 4, and 5.
 #'   star 5 indicates the best hospital in USA.
 #' @seealso \code{\link{rating}}
 #' @export
-rating.data.frame <- function(x,method = c("rclus","kmeans","na"),score_col="sum_score_win") {
+rating.data.frame <- function(x,method = c("rclus","kmeans","na"),score_col="sum_score_win",iter.max=100) {
     if (exists(score_col,x))
     {
-        fit = rating(x[,score_col],method=method)
+        fit = rating(x[,score_col],method=method,iter.max=iter.max)
     } else {
         stop("rating: the input score column doesn't match!")
     }
-    fit$summary_score <- cbind.data.frame(x,fit$star)
+    star <- fit$star
+    fit$summary_score <- cbind.data.frame(x,star)
     fit
 }
