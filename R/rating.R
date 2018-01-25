20bf43a05f3407c13e18041894b4db70674bf404
Ren-Huai Huang <ren-huai.huang@ascension.org>
2018-01-24 21:37:44 -0600

    add rclus2: two step clustering with strict mode

--- R/rating.R
+++ R/rating.R
@@ -17,7 +17,7 @@ rating <- function(x,method,score_col,iter.max) UseMethod("rating")
 #' @rdname rating
 #'
 #' @export
-rating.default <- function(x, method = c("rclus","kmeans","na"),iter.max=1000) {
+rating.default <- function(x, method = c("rclus2","rclus","kmeans","na"),iter.max=1000) {
     if (is.null(x)) return(NULL);
 
     star <- switch(method[1],
@@ -27,8 +27,21 @@ rating.default <- function(x, method = c("rclus","kmeans","na"),iter.max=1000) {
                        fit$star
                    },
                    rclus={
-                       fit = rclus(x,seeds=5,maxiter = iter.max)
+                       fit = rclus(x,seeds=5,maxiter = iter.max,strict = NULL)
                        fit$cluster
+                   },
+                   # for 2017 Dec SAS Pack
+                   rclus2={
+                       groups <- cut(x,breaks=c(quantile(x,probs = seq(0,1,by=0.2))),
+                                     labels=paste0(seq(0,80,20),"-",seq(20,100,20)),include.lowest = TRUE)
+                       # step 1: generate seeds
+                       cat("\n","Step 1: ")
+                       cl1   <- rclus(x, seeds=tapply(x,groups,median), maxiter=iter.max, strict=NULL)
+
+                       # step 2: use the seeds from step 1 and use strict mode
+                       cat("\n","Step 2:")
+                       fit   <- rclus(x, seeds=cl1$centers, maxiter=iter.max, strict=1)
+
                    },
                    na={
                        fit=NULL
@@ -50,7 +63,8 @@ rating.default <- function(x, method = c("rclus","kmeans","na"),iter.max=1000) {
 #'   star 5 indicates the best hospital in USA.
 #' @seealso \code{\link{rating}}
 #' @export
-rating.data.frame <- function(x,method = c("rclus","kmeans","na"),score_col="sum_score_win",iter.max=100) {
+rating.data.frame <- function(x,method = c("rclus2","rclus","kmeans","na"),
+                              score_col="sum_score_win",iter.max=1000) {
     if (exists(score_col,x))
     {
         fit = rating(x[,score_col],method=method,iter.max=iter.max)
--- R/rclus.R
+++ R/rclus.R
@@ -3,14 +3,18 @@
 #' Rapid Clustering. It works for One Dimensional array data only currently.
 #'
 #' @param dat An one-dimension array data.
-#' @param seeds The number of start seeds.
+#' @param seeds The number of start seeds or a vector of centroids (or cluster centers).
 #' @param maxiter The maximum number of iteration specified.
+#' @param strict Strict value. It prevents an observation from being assigned to
+#'   a cluster if its distance to the nearest cluster seed exceeds the value of
+#'   the STRICT. Set strict = NULL to disable the strict mode.
+#'
 #' @return A clustering object including clusters, actual iteration number,
 #'   cluster centers and the size of each cluster, etc.
 #'
 #' @export
 #'
-rclus <- function(dat,seeds=5, maxiter = 1000) {
+rclus <- function(dat,seeds=5, maxiter = 1000, strict =NULL) {
 
     if (missing(dat) | is.null(dat)) stop("input data is missing!")
 
@@ -18,12 +22,12 @@ rclus <- function(dat,seeds=5, maxiter = 1000) {
     # Seeds initialization
     #----------------------
     if (length(seeds) > 1) {
-        cat("seeds:",seeds,"\n")
+        cat("\n","seeds:",seeds,"\n")
         init_seeds <- centers <- seeds
     } else if (length(seeds) == 1 & is.integer(seeds)) {
         init_seeds <- centers <- seeds_init(dat, num_centers=seeds)
     } else {
-        stop("check your seeds")
+        stop("check the seeds")
     }
 
 
@@ -40,7 +44,14 @@ rclus <- function(dat,seeds=5, maxiter = 1000) {
         }
 
         # update the centroids
-        centers <- sort(tapply(dat,clusters,mean))
+        if (is.null(strict) || strict < 0) {
+            centers <- sort(tapply(dat,clusters,mean))
+        } else {
+            # replicate SAS fastclus strict mode
+            strict_index <- (abs(dat-centers[clusters]) < strict)
+            centers <- sort(tapply(dat[strict_index],clusters[strict_index],mean))
+        }
+
 
         #
         # iter_history[iter,] <- round(abs((centers - old_centers)/old_centers),6)
@@ -52,6 +63,7 @@ rclus <- function(dat,seeds=5, maxiter = 1000) {
         }
     }
 
+    cat("\n","Cnter:",centers)
     #---------------------------------------------------------------------------
     # Final assignment of the data to the nearest/closest updated centroid
     #---------------------------------------------------------------------------
